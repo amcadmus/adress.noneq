23d86740246d690a785a93507d73202949b08f54
Han Wang <han.wang@fu-berlin.de>
2012-05-07 20:54:53 +0200

    debug, bug free

--- methane.solv/template/tools/h.bond/app/test.cpp
+++ methane.solv/template/tools/h.bond/app/test.cpp
@@ -68,17 +68,17 @@ int main(int argc, char * argv[])
   // }
   // printf ("\n");
 
-  // map.push_pair (0,1);
-  // map.push_pair (0,2);
-  // map.push_pair (1,3);
-  // map.push_pair (2,3);
-  // map.push_pair (2,4);
-  // map.push_pair (3,5);
-  // map.push_pair (4,5);
-  // map.push_pair (4,6);
-  // map.push_pair (5,7);
-  // map.push_pair (6,7);
-  // map.push_pair (6,7);
+  map.push_pair (0,1);
+  map.push_pair (0,2);
+  map.push_pair (1,3);
+  map.push_pair (2,3);
+  map.push_pair (2,4);
+  map.push_pair (3,5);
+  map.push_pair (4,5);
+  map.push_pair (4,6);
+  map.push_pair (5,7);
+  map.push_pair (6,7);
+  map.push_pair (6,7);
   // for (unsigned i = 0; i < map.nNodes(); ++i){
   //   if (map.findNeighbors (i, neigbors)){
   //     printf ("%d      ", i);
@@ -90,18 +90,18 @@ int main(int argc, char * argv[])
   // }
   // printf ("\n");
 
-  map.push_pair (0,1);
-  map.push_pair (0,2);
-  map.push_pair (1,3);
-  map.push_pair (2,3);
-  map.push_pair (4,3);
-  map.push_pair (4,5);
-  map.push_pair (4,6);
-  map.push_pair (5,7);
-  map.push_pair (6,8);
-  map.push_pair (7,8);
-  map.push_pair (7,9);
-  map.push_pair (8,10);
+  // map.push_pair (0,1);
+  // map.push_pair (0,2);
+  // map.push_pair (1,3);
+  // map.push_pair (2,3);
+  // map.push_pair (4,3);
+  // map.push_pair (4,5);
+  // map.push_pair (4,6);
+  // map.push_pair (5,7);
+  // map.push_pair (6,8);
+  // map.push_pair (7,8);
+  // map.push_pair (7,9);
+  // map.push_pair (8,10);
 
   Tree tree;
   tree.addRoot(0);
@@ -117,7 +117,10 @@ int main(int argc, char * argv[])
   tree.buildCircles (c);
   c.uniqueCircles();
   c.print ();
-  
+
+  std::vector<std::vector<unsigned > > p0, p1;
+  CircleOperations::findCommonPatterns (c.circles[0], c.circles[1],
+					p0, p1);
   
   return 0;
 }
