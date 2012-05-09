c2ac46e9815816fb5ff6974ed0a5a283b22d6f32
Han Wang <han.wang@fu-berlin.de>
2012-05-09 10:43:47 +0200

    report duplicate if one point is contacted

--- methane.solv/template/tools/h.bond/app/analyzeCircle.cpp
+++ methane.solv/template/tools/h.bond/app/analyzeCircle.cpp
@@ -33,12 +33,22 @@ int main(int argc, char * argv[])
   map.push_pair (3,5);
   map.push_pair (4,0);
   map.push_pair (5,1);
-  
-  map.push_pair (101, 102);
-  map.push_pair (101, 103);
-  map.push_pair (102, 103);
 
-  map.push_pair (202, 203);
+  // map.push_pair (0, 1);
+  // map.push_pair (0, 2);
+  // map.push_pair (0, 3);
+  // map.push_pair (1, 3);
+  // map.push_pair (2, 3);
+  // map.push_pair (1, 4);
+  // map.push_pair (2, 5);
+  // map.push_pair (4, 6);
+  // map.push_pair (5, 6);
+
+  // map.push_pair (101, 102);
+  // map.push_pair (101, 103);
+  // map.push_pair (102, 103);
+
+  // map.push_pair (202, 203);
 
   Tree tree;
   Circles cs;
@@ -46,10 +56,17 @@ int main(int argc, char * argv[])
   while (!map.empty()){
     tree.addRoot(map, 0);
     tree.addGenerations(map);
+    // tree.print();
     Circles tmpc;
     tree.buildCircles (tmpc);
     tmpc.uniqueCircles();
+    // std::cout << std::endl;
+    // std::cout << "before simplified:" << std::endl;
+    // tmpc.print();
     tmpc.simplifyCircles ();
+    // std::cout << std::endl;
+    // std::cout << "simplified circles:" << std::endl;
+    // tmpc.print();    
     cs.add (tmpc);
     HbondMap newMap;
     tree.renewMap (map, newMap);
