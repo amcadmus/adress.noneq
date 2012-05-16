4725b228e058c67bf636e20d3894a551432d8c95
Han Wang <han.wang@fu-berlin.de>
2012-05-16 17:02:15 +0200

    implement average of file

--- methane.solv/template/tools/average/app/average.file.cpp
+++ methane.solv/template/tools/average/app/average.file.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 #include <cmath>
 #include <boost/program_options.hpp>
+#define MaxLineLength 10240
 
 #include "StringSplit.h"
 namespace po = boost::program_options;
@@ -24,7 +25,7 @@ int main(int argc, char * argv[])
   desc.add_options()
       ("help,h", "print this message")
       ("output,o", po::value<std::string > (&ofile)->default_value ("avg.h.bond.count.out"), "the output of count of h-bond")
-      ("input,f",  po::value<std::string > (&ifile)->default_value ("h.bond.name"), "the file of file names");
+      ("input,f",  po::value<std::string > (&ifile)->default_value ("h.count.name"), "the file of file names");
       
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -34,5 +35,79 @@ int main(int argc, char * argv[])
     return 0;
   }
 
+  std::ifstream fpname (ifile.c_str());
+  if (!fpname){
+    std::cerr << "cannot open file " << ifile << std::endl;
+    return 1;
+  }
+  char nameline [MaxLineLength];
+  std::vector<std::vector<double > > values;
+  unsigned countFile = 0;
+  
+  while (fpname.getline(nameline, MaxLineLength)){
+    if (nameline[0] == '#') continue;
+    std::ifstream fptraj (nameline);
+    if (!fptraj){
+      std::cerr << "cannot open file " << nameline << std::endl;
+      return 1;
+    }
+    countFile ++;
+    if (countFile == 1){
+      char line [MaxLineLength];
+      while (fptraj.getline(line, MaxLineLength)){
+	if (line[0] == '#') continue;
+	std::string thisline (line);
+	std::vector<std::string > words;
+	StringOperation::split (thisline, words);
+	std::vector<double > lineValues;
+	for (unsigned ii = 0; ii < words.size(); ++ii){
+	  lineValues.push_back(atof(words[ii].c_str()));
+	}
+	values.push_back (lineValues);
+      }
+    }
+    else {
+      unsigned lineCount = 0;
+      char line [MaxLineLength];
+      while (fptraj.getline(line, MaxLineLength)){
+	if (line[0] == '#') continue;
+	std::string thisline (line);
+	std::vector<std::string > words;
+	StringOperation::split (thisline, words);
+	std::vector<double > lineValues;
+	for (unsigned ii = 0; ii < words.size(); ++ii){
+	  lineValues.push_back(atof(words[ii].c_str()));
+	}
+	if (lineCount >= values.size()){
+	  std::cerr << "wrong number of lines of file "
+		    << nameline << std::endl;
+	  return 1;
+	}
+	if (lineValues.size() != values[lineCount].size()){
+	  std::cerr << "broken line "
+		    << lineCount
+		    << " of file "
+		    << nameline << std::endl;
+	  return 1;
+	}
+	for (unsigned ii = 1; ii < lineValues.size(); ++ii){
+	  values[lineCount][ii] += lineValues[ii];
+	}
+	lineCount ++;
+      }
+    }
+  }
+
+  FILE * fout = fopen (ofile.c_str(), "w");
+  for (unsigned ii = 0; ii < values.size(); ++ii){
+    fprintf (fout, "%.3f  ", values[ii][0]);
+    for (unsigned jj = 1; jj < values[ii].size(); ++jj){
+      values[ii][jj] /= countFile;
+      fprintf (fout, "%f  ", values[ii][jj]);
+    }
+    fprintf (fout, "\n");
+  }
+  fclose (fout);
+  
   return 0;
 }
