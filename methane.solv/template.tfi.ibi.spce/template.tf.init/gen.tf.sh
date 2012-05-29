29e81a583099aeb7d5a33c867888141d9e119e56
Han Wang <han.wang@fu-berlin.de>
2012-05-29 13:25:36 +0200

    right tf table extension

--- methane.solv/template.tfi.ibi.spce/template.tf.init/gen.tf.sh
+++ methane.solv/template.tfi.ibi.spce/template.tf.init/gen.tf.sh
@@ -105,13 +105,14 @@ tf_max=`echo "$ex_region_r + $hy_region_r + $tf_extension" | bc -l`
 tf_spline_start=`echo "$ex_region_r - $tf_spline_extension" | bc -l`
 tf_spline_end=`  echo "$ex_region_r + $hy_region_r + $tf_spline_extension" | bc -l`
 half_boxx_1=`echo "$half_boxx + 1." | bc -l`
+half_boxx_2=`echo "($half_boxx + 1.) * 1.8" | bc -l`
 sed -e "s/<min>.*<\/min>/<min>$tf_min<\/min>/g" settings.xml |\
 sed -e "s/<max>.*<\/max>/<max>$tf_max<\/max>/g" |\
 sed -e "s/<step>.*<\/step>/<step>$tf_step<\/step>/g" |\
 sed -e "s/<spline_start>.*<\/spline_start>/<spline_start>$tf_spline_start<\/spline_start>/g" |\
 sed -e "s/<spline_end>.*<\/spline_end>/<spline_end>$tf_spline_end<\/spline_end>/g" |\
 sed -e "s/<spline_step>.*<\/spline_step>/<spline_step>$tf_spline_step<\/spline_step>/g" |\
-sed -e "s/<table_end>.*<\/table_end>/<table_end>$half_boxx_1<\/table_end>/g" |\
+sed -e "s/<table_end>.*<\/table_end>/<table_end>$half_boxx_2<\/table_end>/g" |\
 sed -e "s/<prefactor>.*<\/prefactor>/<prefactor>$tf_prefactor<\/prefactor>/g" |\
 sed -e "s/<iterations_max>.*<\/iterations_max>/<iterations_max>$tf_iterations_max<\/iterations_max>/g" > settings.xml.tmp
 mv -f settings.xml.tmp settings.xml
