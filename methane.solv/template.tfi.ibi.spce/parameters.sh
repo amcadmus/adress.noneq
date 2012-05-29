157c036a7cca85e2459080b87f0bdea9d43cbd32
Han Wang <han.wang@fu-berlin.de>
2012-05-29 11:32:45 +0200

    change default setting

--- methane.solv/template.tfi.ibi.spce/parameters.sh
+++ methane.solv/template.tfi.ibi.spce/parameters.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 base_conf=tools/gen.conf/spc216.gro
-n_base_block="4 2 2"
+n_base_block="4 4 4"
 number_density=33.286      # 0.99577 g/cm^3
 cg_pot_file=tools/table_CG_CG.xvg
 tgt_rdf_file=tools/rdf.com.1e-2.xvg
@@ -24,12 +24,12 @@ tf_gmx_nsteps=500000
 tf_gmx_nstenergy=500
 tf_gmx_nstxtcout=500
 
-ex_region_r=0.5
-hy_region_r=2.75
+ex_region_r=0.54
+hy_region_r=2.70
 tf_extension=0.05
 tf_step=0.01
 tf_spline_extension=0.1
-tf_spline_step=0.42142857142857142857 # 2.95/7
+tf_spline_step=0.41428571428571428571 # 2.9/7
 tf_prefactor=0.039
 
 
