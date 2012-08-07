0d6fedec974556c53f1881b095b99c711742dfbd
Han Wang <han.wang@fu-berlin.de>
2012-08-07 15:27:53 +0200

    add COM for conf file with methane

--- methane.solv/template/tools/gen.conf/app/stupid.add.com.cpp
+++ methane.solv/template/tools/gen.conf/app/stupid.add.com.cpp
@@ -22,13 +22,15 @@ int main (int argc, char * argv[])
 {
   std::string ifilename;
   std::string ofilename;
+  std::string tfile;
   po::options_description desc ("Allow options");  
-
+  int nMeth;
   
   desc.add_options()
       ("help,h", "print this message")
-      ("output-file-name,o", po::value<std::string > (&ofilename)->default_value (std::string("out.gro"), "output conf file name"))
-      ("input-file-name,f",  po::value<std::string > (&ifilename)->default_value (std::string("conf.gro"), "input conf file name"));
+      ("top-file,t",po::value<std::string > (&tfile)->default_value (std::string("mytop")), "topolgy of the system")
+      ("output-file-name,o", po::value<std::string > (&ofilename)->default_value (std::string("out.gro")), "output conf file name")
+      ("input-file-name,f",  po::value<std::string > (&ifilename)->default_value (std::string("conf.gro")), "input conf file name");
 
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -38,6 +40,13 @@ int main (int argc, char * argv[])
     std::cout << desc<< "\n";
     return 0;
   }
+
+  FILE * fp = fopen (tfile.c_str(), "r");
+  if (fp == NULL){
+    std::cerr << "cannot open top file: " << tfile << std::endl;
+    exit (1);
+  }
+  fscanf (fp, "%d", &nMeth);
 
   std::vector<int >  resdindex;
   std::vector<std::string >   resdname;
@@ -57,63 +66,72 @@ int main (int argc, char * argv[])
   std::vector<std::vector<double > >  posi1;
   std::vector<std::vector<double > >  velo1;
 
-  unsigned nmol = posi.size() / 3;
-  resdindex1.resize(nmol*4);
-  atomindex1.resize(nmol*4);
-  resdname1.resize(nmol*4);
-  atomname1.resize(nmol*4);
-  posi1.resize(nmol*4);
-  velo1.resize(nmol*4);
+  unsigned nmol = (posi.size() - nMeth) / 3;
+  resdindex1.resize(nMeth+nmol*4);
+  atomindex1.resize(nMeth+nmol*4);
+  resdname1.resize(nMeth+nmol*4);
+  atomname1.resize(nMeth+nmol*4);
+  posi1.resize(nMeth+nmol*4);
+  velo1.resize(nMeth+nmol*4);
   
-  unsigned countAtom = 0;
+  unsigned countAtom = nMeth;
   std::vector<double > mass (3, 1.00800);
   mass[0] = 15.99940;
   double totalmass = mass[0] + mass[1] + mass[2];
 
+  std::cout << "nMeth is " << nMeth << std::endl;
   std::cout << "nmol is " << nmol << std::endl;
   std::cout << "size is " << atomname.size() << std::endl;
   
   for (unsigned i = 0; i < nmol; ++i){
-    resdindex1[4*i+0] = resdindex[3*i+0];
-    resdindex1[4*i+1] = resdindex[3*i+0];
-    resdindex1[4*i+2] = resdindex[3*i+0];
-    resdindex1[4*i+3] = resdindex[3*i+0];
-    atomindex1[4*i+0] = ++ countAtom;
-    atomindex1[4*i+1] = ++ countAtom;
-    atomindex1[4*i+2] = ++ countAtom;
-    atomindex1[4*i+3] = ++ countAtom;
-    resdname1[4*i+0] = resdname[3*i+0];
-    resdname1[4*i+1] = resdname[3*i+0];
-    resdname1[4*i+2] = resdname[3*i+0];
-    resdname1[4*i+3] = resdname[3*i+0];
-    atomname1[4*i+0] = atomname[3*i+0];
-    atomname1[4*i+1] = atomname[3*i+1];
-    atomname1[4*i+2] = atomname[3*i+2];
-    atomname1[4*i+3] = "COM";
+    resdindex1[nMeth+4*i+0] = resdindex[nMeth+3*i+0];
+    resdindex1[nMeth+4*i+1] = resdindex[nMeth+3*i+0];
+    resdindex1[nMeth+4*i+2] = resdindex[nMeth+3*i+0];
+    resdindex1[nMeth+4*i+3] = resdindex[nMeth+3*i+0];
+    atomindex1[nMeth+4*i+0] = ++ countAtom;
+    atomindex1[nMeth+4*i+1] = ++ countAtom;
+    atomindex1[nMeth+4*i+2] = ++ countAtom;
+    atomindex1[nMeth+4*i+3] = ++ countAtom;
+    resdname1[nMeth+4*i+0] = resdname[nMeth+3*i+0];
+    resdname1[nMeth+4*i+1] = resdname[nMeth+3*i+0];
+    resdname1[nMeth+4*i+2] = resdname[nMeth+3*i+0];
+    resdname1[nMeth+4*i+3] = resdname[nMeth+3*i+0];
+    atomname1[nMeth+4*i+0] = atomname[nMeth+3*i+0];
+    atomname1[nMeth+4*i+1] = atomname[nMeth+3*i+1];
+    atomname1[nMeth+4*i+2] = atomname[nMeth+3*i+2];
+    atomname1[nMeth+4*i+3] = "COM";
 
     std::vector<double > composi(3, 0.);
     std::vector<double > comvelo(3, 0.);
     
     for (unsigned jj = 0; jj < 3; ++jj){
       for (unsigned dd = 0; dd < 3; ++dd){
-	composi[dd] += mass[jj] * posi[3*i+jj][dd];
-	comvelo[dd] += mass[jj] * velo[3*i+jj][dd];
+	composi[dd] += mass[jj] * posi[nMeth+3*i+jj][dd];
+	comvelo[dd] += mass[jj] * velo[nMeth+3*i+jj][dd];
       }
     }
     for (unsigned dd = 0; dd < 3; ++dd){
       composi[dd] /= totalmass;
       comvelo[dd] /= totalmass;
     }
-    posi1[4*i+0] = posi[3*i+0];
-    posi1[4*i+1] = posi[3*i+1];
-    posi1[4*i+2] = posi[3*i+2];
-    posi1[4*i+3] = composi;
-    velo1[4*i+0] = velo[3*i+0];
-    velo1[4*i+1] = velo[3*i+1];
-    velo1[4*i+2] = velo[3*i+2];
-    velo1[4*i+3] = comvelo;
+    posi1[nMeth+4*i+0] = posi[nMeth+3*i+0];
+    posi1[nMeth+4*i+1] = posi[nMeth+3*i+1];
+    posi1[nMeth+4*i+2] = posi[nMeth+3*i+2];
+    posi1[nMeth+4*i+3] = composi;
+    velo1[nMeth+4*i+0] = velo[nMeth+3*i+0];
+    velo1[nMeth+4*i+1] = velo[nMeth+3*i+1];
+    velo1[nMeth+4*i+2] = velo[nMeth+3*i+2];
+    velo1[nMeth+4*i+3] = comvelo;
   }  
-  
+
+  for (int ii = 0; ii < nMeth; ++ii){
+    resdindex1[ii] = resdindex[ii];
+    atomindex1[ii] = atomindex[ii];
+    resdname1[ii] = resdname[ii];
+    atomname1[ii] = atomname[ii];
+    posi1[ii] = posi[ii];
+    velo1[ii] = velo[ii];
+  }
   
   GroFileManager::write (ofilename,
 			 resdindex1, resdname1,
