87b505cf6b53588b2923bb817e5c2463383fdd87
Han Wang <han.wang@fu-berlin.de>
2012-08-07 17:45:43 +0200

    add support for adress

--- methane.solv/template/run.equi.sh
+++ methane.solv/template/run.equi.sh
@@ -12,8 +12,10 @@ cp -a $gro_dir result.equi
 cd result.equi
 set_parameters_equi grompp.mdp
 
-grompp
-mdrun -v
+echo "# run with command `which grompp`"
+$grompp_command
+echo "# run with command `which mdrun`"
+$mdrun_command
 
 split_trr
 cd ..
