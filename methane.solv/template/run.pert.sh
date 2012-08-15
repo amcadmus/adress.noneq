31c8c79ac89d28bcca05d617b44df74e5dc9e9db
Han Wang <han.wang@fu-berlin.de>
2012-08-15 10:55:31 +0200

    revised run pert. del traj and cal h bond during run

--- methane.solv/template/run.pert.sh
+++ methane.solv/template/run.pert.sh
@@ -36,6 +36,17 @@ do
     $grompp_command &>> run.log
     echo "# run with command `which mdrun`" &>> run.log
     $mdrun_command &>> run.log
+
+    if test ! -f mytop; then
+	echo "old version, no mytop"
+	../tools/h.bond/equi.ch4.1 -o h.count.out &> /dev/null
+    else
+	../tools/h.bond/equi.ch4.1 -t mytop -o h.count.out &> /dev/null
+    fi
+    rm -f traj.xtc state*.cpt topol.tpr
+    
     cd ..
+    
+    echo "$my_dir/h.count.out" >> h.count.name    
 done
 
--- methane.solv/template/tools/scripts/h.bond.count.sh
+++ methane.solv/template/tools/scripts/h.bond.count.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 targets=`ls | grep "^pert"`
-rm -f h.count.name
+# rm -f h.count.name
 
 make -C tools/h.bond makedir
 make -C tools/h.bond -j 4
 make -C tools/average
 make -C tools/average -j 4
 
-for i in $targets;
-do
-    echo "# processing $i"
-    cd $i
-    if test ! -f mytop; then
-	echo "old version, no mytop"
-	../tools/h.bond/equi.ch4.1 -o h.count.out &> /dev/null
-    else
-	../tools/h.bond/equi.ch4.1 -t mytop -o h.count.out &> /dev/null
-    fi
-    cd ..
-    echo "$i/h.count.out" >> h.count.name
-done
+# for i in $targets;
+# do
+#     echo "# processing $i"
+#     cd $i
+#     if test ! -f mytop; then
+# 	echo "old version, no mytop"
+# 	../tools/h.bond/equi.ch4.1 -o h.count.out &> /dev/null
+#     else
+# 	../tools/h.bond/equi.ch4.1 -t mytop -o h.count.out &> /dev/null
+#     fi
+#     cd ..
+#     echo "$i/h.count.out" >> h.count.name
+# done
 
 ./tools/average/average.file
 
