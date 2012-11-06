6297d09c2cc1467bb8025118cf51f892fbb988c3
Han Wang <han.wang@fu-berlin.de>
2012-11-06 11:05:05 +0100

    log equi running. better angle script

--- alanine.nanma/template/run.equi.sh
+++ alanine.nanma/template/run.equi.sh
@@ -2,6 +2,7 @@
 
 source parameters.sh
 source functions.sh
+rm -f run.log
 
 if test -d result.equi; then
     echo "existing dir result.equi, mv and backup"
@@ -17,9 +18,9 @@ cd result.equi
 set_parameters_equi grompp.mdp
 
 echo "# run with command `which grompp`"
-$grompp_command
+$grompp_command &>> run.log
 echo "# run with command `which mdrun`"
-$mdrun_command
+$mdrun_command &>> run.log
 
 split_trr
 cd ..
--- alanine.nanma/template/tools/scripts/angle.avg.sh
+++ alanine.nanma/template/tools/scripts/angle.avg.sh
@@ -2,7 +2,19 @@
 
 make -C ./tools/angles/ -j
 
-./tools/angles/average.traj -f angle.name -o avg.angle
+cwd=`pwd`
+
+for i in `cat angle.name`; 
+do
+    if test ! -f $i; then
+	echo "# no file $i, make it"
+	cd $(dirname $i)
+	$cwd/tools/angles/evolve -f alanine.xtc -s angle.dat > /dev/null
+	cd $cwd
+    fi
+done
+
+./tools/angles/average.traj -f angle.name -o avg.angle --refh 5
 
 test ! -d dir.avg.angle && mkdir dir.avg.angle
 
