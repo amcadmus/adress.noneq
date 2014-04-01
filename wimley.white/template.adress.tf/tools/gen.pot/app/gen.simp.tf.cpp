b9f076573b870aaec63b70a61bafe639c8a58559
amcadmus <amcadmus@gmail.com>
2014-04-01 18:55:01 +0200

    done

--- wimley.white/template.adress.tf/tools/gen.pot/app/gen.simp.tf.cpp
+++ wimley.white/template.adress.tf/tools/gen.pot/app/gen.simp.tf.cpp
@@ -23,20 +23,20 @@ int main(int argc, char * argv[])
 {
   double xup;
   double bin;
-  double ucap;
-  double eps;
-  double sig;
+  double xex;
+  double xhy;
+  double pot0;
   string ofile;
   
   po::options_description desc ("Allow options");
   desc.add_options()
     ("help,h", "print this message")
-    ("epsilon,e", po::value<double > (&eps)->default_value (0.650194),  "epsilon")
-    ("sigma,s",   po::value<double > (&sig)->default_value (0.316557),  "epsilon")
+    ("xup,x", po::value<double > (&xup)->default_value (10.0), "up of x")
     ("bin,b", po::value<double > (&bin)->default_value (0.002), "size of the bin")
-    ("xup,x", po::value<double > (&xup)->default_value (2.0), "up of x")
-    ("ucap,u", po::value<double > (&ucap)->default_value (1e6), "cap value of energy")
-    ("output,o",   po::value<std::string > (&ofile)->default_value ("table_CG_CG.xvg"), "output file");
+    ("xex", po::value<double > (&xex)->default_value (1.0), "xex")
+    ("xhy", po::value<double > (&xhy)->default_value (2.0), "xhy")
+    ("pot0,p", po::value<double > (&pot0)->default_value (10.0), "potential at x = 0")
+    ("output,o",   po::value<std::string > (&ofile)->default_value ("tabletf_CG.xvg"), "output file");
   
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -54,48 +54,20 @@ int main(int argc, char * argv[])
     exit (1);
   }
 
-  double rcut = pow (2., 1./6.) * sig;
-
-  fprintf (fp, "# gen with eps: %f, sigma: %f\n",
-	   eps, sig);
-  // fprintf (fp, "%e   %e %e   %e %e   %e %e\n",
-  // 	   0., 0., 0., 0., 0., ucap, 0.);
-  double fcap;
-  double rcap;
   for (int ii = 0; ii < nx; ++ii){
     double tmpx = ii * bin;
-    rcap = tmpx;
-    double tmpr6 = sig / tmpx;
-    tmpr6 = tmpr6 * tmpr6 * tmpr6 * tmpr6 * tmpr6 * tmpr6;
-    double tmpu = 4. * eps * (tmpr6 * tmpr6 - tmpr6) + eps;
-    fcap = 4 * eps * (tmpr6 * tmpr6 * 12. / tmpx - 6. * tmpr6 / tmpx);
-    if (tmpu <= ucap) break;
-  }
-
-  for (int ii = 0; ii < nx; ++ii){
-    double tmpx = ii * bin;
-    // if (ii == 0){
-    //   tmpx = 0.5 * ii * bin;
-    // }
-    double tmpr6 = sig / tmpx;
-    tmpr6 = tmpr6 * tmpr6 * tmpr6 * tmpr6 * tmpr6 * tmpr6;
-    double tmpu = 4. * eps * (tmpr6 * tmpr6 - tmpr6) + eps;
-    double tmpf = 0.;
-    if (tmpu > ucap || ii == 0){
-      tmpu = ucap + fcap * (rcap - tmpx);
-      tmpf = fcap;
+    if (tmpx <= xex){      
+      fprintf (fp, "%e   %e %e\n", tmpx, pot0, 0.);
+    }
+    else if (tmpx >= xhy){
+      fprintf (fp, "%e   %e %e\n", tmpx, 0., 0.);
     }
     else {
-      tmpf = 4 * eps * (tmpr6 * tmpr6 * 12. / tmpx - 6. * tmpr6 / tmpx);
+      fprintf (fp, "%e   %e %e\n", tmpx, 
+	       0.5 * (cos ((tmpx - xex) / (xhy - xex) * M_PI ) + 1) * pot0,
+	       0.5 * (sin ((tmpx - xex) / (xhy - xex) * M_PI )) * pot0 * M_PI / (xhy - xex));
     }
-    if (tmpx >= rcut){
-      tmpu = 0.;
-      tmpf = 0.;
-    }
-    fprintf (fp, "%e   %e %e   %e %e   %e %e\n",
-	     tmpx, 0., 0., 0., 0., tmpu, tmpf);
   }
-  
   
   fclose (fp);
   
