d54532de36a9246f30511c857cb95b4d39668610
amcadmus <amcadmus@gmail.com>
2014-04-06 21:17:29 +0200

    improve template. write sh scripts

---
+++ zm/template.water/parameters.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# cal volumn
+gmx_init_conf=./tools/confs/conf.013k.gro
+gmx_sys_tempalte_dir=./tools/system
+gmx_dt=0.001
+gmx_time=500		# in ps
+gmx_conf_feq=20		# in ps
+gmx_energy_feq=1	# in ps
+gmx_ele_method=pme	# pme or zm
+gmx_rlist=1.5
+gmx_rcut_ele=1.2
+gmx_rcut_ele_switch=1.0
+gmx_rcut_vdw=1.2
+gmx_rcut_vdw_switch=1.0
+gmx_tab_ext=0.5
+
+zm_l=2
+zm_alpha=0.1
+
+gmx_grompp_command="grompp"
+gmx_mdrun_command="mdrun"
