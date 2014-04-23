2e48427bef8066f15370c953eed2cd8325e5645c
becwangh <becwangh@hlogin1.hsn.hlrn.de>
2014-04-23 16:17:32 +0200

    better assigning parameters. no tune pme by gromacs routine

--- zm/template.water/assign.param.sh
+++ zm/template.water/assign.param.sh
@@ -3,29 +3,31 @@
 cwd=`pwd`
 dirname=${cwd##*/}
 
-ll=`echo $dirname | cut -f 2 -d '.' | cut -f 2 -d 'l'`
-alpha1=`echo $dirname | cut -f 3 -d '.' | cut -f 3 -d 'a'`
-alpha2=`echo $dirname | cut -f 4 -d '.'`
-alpha=$alpha1.$alpha2
-rc1=`echo $dirname | cut -f 5 -d '.' | cut -f 2 -d 'c'`
-rc2=`echo $dirname | cut -f 6 -d '.'`
-rc=$rc1.$rc2
+method=`echo $dirname | cut -f 1 -d '.' `
+if echo $method | grep zm; then
+    ll=`echo $dirname | cut -f 2 -d '.' | cut -f 2 -d 'l'`
+    alpha1=`echo $dirname | cut -f 3 -d '.' | cut -f 3 -d 'a'`
+    alpha2=`echo $dirname | cut -f 4 -d '.'`
+    alpha=$alpha1.$alpha2
+    rc1=`echo $dirname | cut -f 5 -d '.' | cut -f 2 -d 'c'`
+    rc2=`echo $dirname | cut -f 6 -d '.'`
+    rc=$rc1.$rc2
 
-source parameters.sh
-if test `echo "$gmx_rcut_vdw" '>' "$rc" | bc` -eq 1; then
-    maxrc=$gmx_rcut_vdw
-else
-    maxrc=$rc
-fi
-
-rlist=`echo "$maxrc+0.3" | bc -l`
-
-cat parameters.sh |\
+    source parameters.sh
+    if test `echo "$gmx_rcut_vdw" '>' "$rc" | bc` -eq 1; then
+	maxrc=$gmx_rcut_vdw
+    else
+	maxrc=$rc
+    fi
+    
+    rlist=`echo "$maxrc+0.3" | bc -l`
+    
+    cat parameters.sh |\
 sed -e "s/zm_l=.*/zm_l=$ll/g" |\
 sed -e "s/zm_alpha=.*/zm_alpha=$alpha/g" |\
 sed -e "s/gmx_rlist=.*/gmx_rlist=$rlist/g" |\
 sed -e "s/gmx_rcut_ele=.*/gmx_rcut_ele=$rc/g" \
-    > tmp.param
-
-mv -f tmp.param parameters.sh
-
+	> tmp.param
+    
+    mv -f tmp.param parameters.sh
+fi
--- zm/template.water/env.fu.sh
+++ zm/template.water/env.fu.sh
@@ -7,4 +7,4 @@ zm_gen_dir=$HOME/study/adress.noneq.bk/zm/tools/gen.pot
 
 gmx_grompp_command="grompp"
 gmx_tune_command="g_pme_error"
-gmx_mdrun_command="mdrun -nt $nnodes"
+gmx_mdrun_command="mdrun -nt $nnodes -notunepme"
