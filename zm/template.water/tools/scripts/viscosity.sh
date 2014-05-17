389f6adf77cf8d3b9ab5b5c953d1ae525b8eab3e
amcadmus <amcadmus@gmail.com>
2014-05-17 16:59:49 +0200

    improve

--- zm/template.water/tools/scripts/viscosity.sh
+++ zm/template.water/tools/scripts/viscosity.sh
@@ -31,17 +31,23 @@ volume=`echo "$box * $box * $box" | bc -l`
 temperature=300
 scale=`echo "$volume / 1.38 / $temperature * 0.0001" | bc -l`
 
+echo "# auto corr and integrate for xy"
 $acc_command -f tmp.out -u 20 --column-data 2 --num-data-block 1024
 $int_command --scale $scale -f autoCorrelation.out -d 2 -e 3 -i 20 > integrate.xy.out
+echo "# auto corr and integrate for xz"
 $acc_command -f tmp.out -u 20 --column-data 3 --num-data-block 1024
 $int_command --scale $scale -f autoCorrelation.out -d 2 -e 3 -i 20 > integrate.xz.out
+echo "# auto corr and integrate for yz"
 $acc_command -f tmp.out -u 20 --column-data 4 --num-data-block 1024
 $int_command --scale $scale -f autoCorrelation.out -d 2 -e 3 -i 20 > integrate.yz.out
+echo "# auto corr and integrate for (x - y)/2"
 $acc_command -f tmp.out -u 20 --column-data 5 --num-data-block 1024
 $int_command --scale $scale -f autoCorrelation.out -d 2 -e 3 -i 20 > integrate.xmy.out
+echo "# auto corr and integrate for (y - z)/2"
 $acc_command -f tmp.out -u 20 --column-data 6 --num-data-block 1024
 $int_command --scale $scale -f autoCorrelation.out -d 2 -e 3 -i 20 > integrate.ymz.out
 
+echo "# join results"
 cp integrate.xy.out integrate.all.out
 join integrate.all.out integrate.xz.out > tmp.out; mv -f tmp.out integrate.all.out
 join integrate.all.out integrate.yz.out > tmp.out; mv -f tmp.out integrate.all.out
