484f7ec1e55f8d0032baa47a3fc40161b6874519
amcadmus <amcadmus@gmail.com>
2014-06-05 17:45:06 +0200

    change default parameter setting

---
+++ wimley.white/template.simul/tools/scripts/mm.alanine.parameters.sh
@@ -0,0 +1,14 @@
+
+redo=0
+nbins=20
+lag_times="`seq 1 1 20` `seq 22 2 40`"
+#lag_times="`seq 1 1 3`"
+selected_lag_time=10
+num_eig=5
+err_num_samples=8
+
+targets=`ls | grep ^simul | grep [0-9]$`
+traj_dir=gromacs.traj.npt
+phi_file_name=angaver.phi.xvg
+psi_file_name=angaver.psi.xvg
+
