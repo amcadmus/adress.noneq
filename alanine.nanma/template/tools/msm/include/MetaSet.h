8560053ba57038f190b9dc88c8b36c89486f82c2
amcadmus <amcadmus@gmail.com>
2014-06-12 17:55:21 +0200

    add missing files

---
+++ alanine.nanma/template/tools/msm/include/MetaSet.h
@@ -0,0 +1,19 @@
+#ifndef __MetaSet_h_wanghan__
+#define __MetaSet_h_wanghan__
+
+class MetastableSet 
+{
+private:
+  double phi_b, phi_e;
+  double psi_b, psi_e;
+public:
+  MetastableSet (const double & phb,
+		 const double & phe,
+		 const double & psb,
+		 const double & pse);
+  bool inSet (const double & phi,
+	      const double & psi) const;
+}
+    ;
+
+#endif
