377d613f5a78a0f74ffc622882802a721b0efbda
amcadmus <amcadmus@gmail.com>
2014-06-16 10:22:37 +0200

    add missing file

---
+++ wimley.white/template.adress.tf/env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+votca_install_dir=~/study/chemical.potential/local
+source $votca_install_dir/bin/VOTCARC.bash
+
+gromacs_install_dir=~/study/chemical.potential/local.4.6.3
+source $gromacs_install_dir/bin/GMXRC.bash
+
