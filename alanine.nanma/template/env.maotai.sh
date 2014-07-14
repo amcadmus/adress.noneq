7ed825e483001a426bc666be8491ee06ddb208fa
amcadmus <amcadmus@gmail.com>
2014-07-14 14:49:40 +0200

    add missing env file

---
+++ alanine.nanma/template/env.maotai.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gmx_dir=$HOME/local/gromacs/4.6.5.localsd.fcorr
+source $gmx_dir/bin/GMXRC.bash
+
+zm_gen_dir=$WORK/study/adress.noneq/zm/tools/gen.pot
+
+gmx_grompp_command="grompp"
+gmx_tune_command="g_pme_error"
+gmx_mdrun_command="mdrun -notunepme"
