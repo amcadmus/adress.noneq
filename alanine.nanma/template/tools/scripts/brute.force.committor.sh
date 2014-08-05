8266f43be7d93eb40330c287655280ab28a17f11
amcadmus <amcadmus@gmail.com>
2014-08-05 17:40:27 +0200

    add committor scripts

---
+++ alanine.nanma/template/tools/scripts/brute.force.committor.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "usage:"
+    echo "$0 DIR_STORING_MSM"
+    exit
+fi
+
+target_dir=$1
+script_dir=$(cd ${0%/*} && echo $PWD)
+base_dir=$script_dir/..
+angle_dir=$base_dir/angles
+msm_dir=$base_dir/msm
+cwd=`pwd`
+
+if [ ! -d $target_dir ]; then
+    echo "no dir $target_dir"
+    exit
+fi
+
+if [ ! -f $target_dir/msm.parameters.sh ]; then
+    echo "no file $target_dir/msm.parameters.sh"
+    exit
+else
+    source $target_dir/msm.parameters.sh
+fi
+
+mycommand="$msm_dir/calculate.commitor --num-bin $msm_dih_nbin --input-cluster-map $target_dir/cluster.map.out --input-traj-dir dir.name --input-disc-traj traj.dih.disc.periodT --output-fw $target_dir/commitor.fw.out --output-bw $target_dir/commitor.bw.out"
+echo "# calculate commitor by command: $mycommand"
+$mycommand
+
+
