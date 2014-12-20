5ec66062b399e2f43c5088f1f1c936f6a529421e
amcadmus <amcadmus@gmail.com>
2014-12-20 03:24:35 +0100

    update the test script

--- alanine.nanma/template/tools/scripts/msm.analyze.brute.force.chapman-test.sh
+++ alanine.nanma/template/tools/scripts/msm.analyze.brute.force.chapman-test.sh
@@ -40,32 +40,12 @@ test_factor=2
 #     sed -n "1~${period_frame}p" $ii/traj.dih.disc | head -n $n_head_line | tail -n $n_tail_line > $ii/traj.dih.disc.period${test_factor}T
 # done
 
-mycommand="$msm_dir/calculate.commitor --input-largest-set $target_dir/largestSet.dih --num-bin $msm_dih_nbin --input-cluster-map $target_dir/cluster.map.out --input-traj-dir dir.name --input-disc-traj traj.dih.disc.period${test_factor}T --output-fw $target_dir/commitor.fw.out --output-bw $target_dir/commitor.bw.out"
-echo "# calculate commitor by command: $mycommand"
-$mycommand
-paste $target_dir/commitor.fw.out $target_dir/commitor.bw.out > $target_dir/commitor.out
-
-mycommand="$msm_dir/calculate.steady.dist --input-largest-set $target_dir/largestSet.dih --num-bin $msm_dih_nbin --input-traj-dir dir.name --input-disc-traj traj.dih.disc.period${test_factor}T --output $target_dir/steady.dist.out"
-echo "# calculate steady dist by command: $mycommand"
-$mycommand
-
-mycommand="$msm_dir/calculate.coreset.msm --num-bin $msm_dih_nbin --input-largest-set $target_dir/largestSet.dih  --input-floquet $target_dir/floque.B.out --input-cluster-map $target_dir/cluster.map.out --input-steady $target_dir/steady.dist.out.orig --input-fw $target_dir/commitor.fw.out.orig --input-bw $target_dir/commitor.bw.out.orig --output-tmatrix $target_dir/coreset.tmatrix.out"
-echo "# calculate coreset msm tmatrix by command: $mycommand"
-$mycommand
-
-mycommand="$msm_dir/calculate.coreset.msm.nemd --num-bin $msm_dih_nbin --input traj.dih.disc.period${test_factor}T --input-dir dir.name --n-data-block 100 --input-cluster-map $target_dir/cluster.map.out --output-matrix-t $target_dir/coreset.t.out --output-matrix-m $target_dir/coreset.m.out"
+mycommand="$msm_dir/calculate.coreset.msm.nemd --num-bin $msm_dih_nbin --input traj.dih.disc.period${test_factor}T --input-dir dir.name --n-data-block 100 --input-cluster-map $target_dir/cluster.map.out --output-matrix-t $target_dir/coreset.t.out --output-matrix-m $target_dir/coreset.m.out --output-matrix-p $target_dir/coreset.p.out"
 echo "# calculate coreset t and m matrices by command: $mycommand"
 $mycommand
 
-if [ -f $target_dir/bf.prob.dih.out ]; then
-    sed -n "1~${period_frame}p" $target_dir/bf.prob.dih.out > $target_dir/bf.prob.dih.period${test_factor}T.out
-    mycommand="$msm_dir/calculate.coreset.proj --input $target_dir/bf.prob.dih.period${test_factor}T.out --input-largest-set $target_dir/largestSet.dih --input-bw $target_dir/commitor.bw.out.orig --output $target_dir/coreset.prob.${test_factor}T.out"
-    echo "# calculate the projected time-dep prob. by command: $mycommand"
-    $mycommand
-
-    mycommand="$msm_dir/calculate.coreset.evlove --dt $pert_warm_time --end $pert_time --input-init-prob $target_dir/coreset.prob.${test_factor}T.out --input-tmatrix $target_dir/coreset.tmatrix.out --output $target_dir/coreset.prob.coreset.${test_factor}.out"
-    echo "# calculate the prob from coreset msm by $mycommand"
-    $mycommand
-fi
+mycommand="$msm_dir/calculate.coreset.evlove --dt `echo "$test_factor * $pert_warm_time" | bc -l` --end $pert_time --input-init-prob $target_dir/coreset.prob.out --input-tmatrix $target_dir/coreset.p.out --output $target_dir/coreset.prob.coreset.${test_factor}.out"
+echo "# calculate the prob from coreset msm by $mycommand"
+$mycommand
 
 
